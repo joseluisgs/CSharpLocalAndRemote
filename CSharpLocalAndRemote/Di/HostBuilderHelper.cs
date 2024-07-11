006721755986786a2e2a3f0decc1da2ff3f9815b
José Luis González <joseluisgonsan@gmail.com>
2024-07-11 15:58:33 +0200

    Dependencias!!!

--- CSharpLocalAndRemote/Di/HostBuilderHelper.cs
+++ CSharpLocalAndRemote/Di/HostBuilderHelper.cs
@@ -14,14 +14,6 @@ using Microsoft.Extensions.Options;
 
 namespace CSharpLocalAndRemote.Di;
 
-/**
- * Clase de ayuda para construir el host de la aplicación.
- * El host se configura con los servicios necesarios para la aplicación.
- * Se prepara la configuración de la aplicación y se registran los servicios necesarios.
- * La inyección de dependencias se realiza en esta clase.
- * Se puede leer la configuración y registrar los servicios utilizando el método ConfigureServices.
- * Configurar el log y el manejo de excepciones utilizando el método ConfigureLogging.
- */
 public static class HostBuilderHelper
 {
     public static IHost BuildHost(string[] args)
@@ -32,79 +24,97 @@ public static class HostBuilderHelper
                 config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
                 config.AddJsonFile("appsettings.json", true, true);
             })
-            .ConfigureServices((context, services) =>
-            {
-                // Settings dependencias usando las secciones del archivo appsettings.json
-                services.Configure<DatabaseSettings>(context.Configuration.GetSection("Database"));
-                services.Configure<ApiSettings>(context.Configuration.GetSection("ApiRest"));
-                services.Configure<CacheSettings>(context.Configuration.GetSection("Cache"));
-                services.Configure<RefreshSettings>(context.Configuration.GetSection("Refresh"));
-
-                // Configurar el acceso a la base de datos
-                services.AddSingleton<EntityManager<TenistaEntity>>(provider =>
-                {
-                    var databaseSettings = provider.GetRequiredService<IOptions<DatabaseSettings>>().Value;
-
-                    return new EntityManager<TenistaEntity>(
-                        new TenistasDbContext(
-                            new DbContextOptionsBuilder<TenistasDbContext>()
-                                .UseSqlite("Data Source=" + databaseSettings.Name)
-                                .EnableSensitiveDataLogging()
-                                .Options
-                        )
-                    );
-                });
-
-                // Registrar repositorios
-                services.AddSingleton<ITenistasRepositoryLocal, TenistasRepositoryLocal>(provider =>
-                {
-                    var entityManager = provider.GetRequiredService<EntityManager<TenistaEntity>>();
-                    return new TenistasRepositoryLocal(entityManager.Context);
-                });
-
-                // Registrar Repositorio remoto, tiene una dependencia de ApiSettings
-                services.AddSingleton<ITenistasRepositoryRemote>(provider =>
-                {
-                    var apiSettings = provider.GetRequiredService<IOptions<ApiSettings>>().Value;
-                    return new TenistasRepositoryRemote(RefitClient.CreateClient(apiSettings.Url));
-                });
-
-                // Registrar almacenamientos
-                // Cache
-                services.AddSingleton<ITenistasCache, TenistasCache>(provider =>
-                {
-                    var cacheSettings = provider.GetRequiredService<IOptions<CacheSettings>>().Value;
-                    return new TenistasCache(cacheSettings.Size);
-                });
-
-                // Almacenamiento en ficheros
-                services.AddSingleton<ITenistasStorageCsv, TenistasStorageCsv>();
-                services.AddSingleton<ITenistasStorageJson, TenistasStorageJson>();
-
-                // Registrar notificaciones
-                services.AddSingleton<ITenistasNotifications, TenistasNotifications>();
-
-                // Registrar servicios
-                services.AddSingleton<ITenistasService, TenistasService>(provider =>
-                {
-                    var refreshSettings = provider.GetRequiredService<IOptions<RefreshSettings>>().Value;
-                    var repositoryLocal = provider.GetRequiredService<ITenistasRepositoryLocal>();
-                    var repositoryRemote = provider.GetRequiredService<ITenistasRepositoryRemote>();
-                    var cache = provider.GetRequiredService<ITenistasCache>();
-                    var notifications = provider.GetRequiredService<ITenistasNotifications>();
-                    var storageCsv = provider.GetRequiredService<ITenistasStorageCsv>();
-                    var storageJson = provider.GetRequiredService<ITenistasStorageJson>();
-                    return new TenistasService(
-                        repositoryLocal,
-                        repositoryRemote,
-                        cache,
-                        storageCsv,
-                        storageJson,
-                        notifications,
-                        refreshSettings.Interval * 1000 // Convertir a milisegundos
-                    );
-                });
-            })
+            .ConfigureServices(ConfigureServices)
             .Build();
+    }
+
+    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        RegisterSettings(context, services); // Registro de configuraciones
+        RegisterDatabaseAccess(services); // Registro de acceso a base de datos
+        RegisterRepositories(services); // Registro de repositorios
+        RegisterCache(services); // Registro de cache
+        RegisterStorage(services); // Registro de almacenamiento
+        RegisterNotifications(services); // Registro de notificaciones
+        RegisterTenistasService(services); // Registro de servicio de tenistas
+    }
+
+    private static void RegisterSettings(HostBuilderContext context, IServiceCollection services)
+    {
+        // Configuración de secciones de appsettings.json
+        services.Configure<DatabaseSettings>(context.Configuration.GetSection("Database"));
+        services.Configure<ApiSettings>(context.Configuration.GetSection("ApiRest"));
+        services.Configure<CacheSettings>(context.Configuration.GetSection("Cache"));
+        services.Configure<RefreshSettings>(context.Configuration.GetSection("Refresh"));
+    }
+
+    private static void RegisterDatabaseAccess(IServiceCollection services)
+    {
+        services.AddSingleton<EntityManager<TenistaEntity>>(provider =>
+        {
+            // Configuración de base de datos EntityManager, requerimos DatabaseSettings
+            var databaseSettings = provider.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            return new EntityManager<TenistaEntity>(new TenistasDbContext(
+                new DbContextOptionsBuilder<TenistasDbContext>()
+                    .UseSqlite("Data Source=" + databaseSettings.Name)
+                    .EnableSensitiveDataLogging().Options));
+        });
+    }
+
+    private static void RegisterRepositories(IServiceCollection services)
+    {
+        services.AddSingleton<ITenistasRepositoryLocal, TenistasRepositoryLocal>(provider =>
+        {
+            // Repositorio local de tenistas, requerimos EntityManager
+            var entityManager = provider.GetRequiredService<EntityManager<TenistaEntity>>();
+            return new TenistasRepositoryLocal(entityManager.Context);
+        });
+
+        services.AddSingleton<ITenistasRepositoryRemote>(provider =>
+        {
+            // Repositorio remoto de tenistas, requerimos ApiSettings
+            var apiSettings = provider.GetRequiredService<IOptions<ApiSettings>>().Value;
+            return new TenistasRepositoryRemote(RefitClient.CreateClient(apiSettings.Url));
+        });
+    }
+
+    private static void RegisterCache(IServiceCollection services)
+    {
+        // Cache de tenistas, es una nueva instancia, por eso es Transient
+        services.AddTransient<ITenistasCache, TenistasCache>(provider =>
+        {
+            // Cache de tenistas, requerimos CacheSettings
+            var cacheSettings = provider.GetRequiredService<IOptions<CacheSettings>>().Value;
+            return new TenistasCache(cacheSettings.Size);
+        });
+    }
+
+    private static void RegisterStorage(IServiceCollection services)
+    {
+        // Almacenamiento de tenistas
+        services.AddSingleton<ITenistasStorageCsv, TenistasStorageCsv>();
+        services.AddSingleton<ITenistasStorageJson, TenistasStorageJson>();
+    }
+
+    private static void RegisterNotifications(IServiceCollection services)
+    {
+        services.AddSingleton<ITenistasNotifications, TenistasNotifications>();
+    }
+
+    private static void RegisterTenistasService(IServiceCollection services)
+    {
+        // Servicio de tenistas, con sus dependencias
+        services.AddSingleton<ITenistasService, TenistasService>(provider =>
+        {
+            var refreshSettings = provider.GetRequiredService<IOptions<RefreshSettings>>().Value;
+            var repositoryLocal = provider.GetRequiredService<ITenistasRepositoryLocal>();
+            var repositoryRemote = provider.GetRequiredService<ITenistasRepositoryRemote>();
+            var cache = provider.GetRequiredService<ITenistasCache>();
+            var notifications = provider.GetRequiredService<ITenistasNotifications>();
+            var storageCsv = provider.GetRequiredService<ITenistasStorageCsv>();
+            var storageJson = provider.GetRequiredService<ITenistasStorageJson>();
+            return new TenistasService(repositoryLocal, repositoryRemote, cache, storageCsv, storageJson, notifications,
+                refreshSettings.Interval * 1000);
+        });
     }
 }
